d610023eaafd7ec85d3e8b35e31fb7390cf87239
RoopeSavolainen <roope.savolainen@gmail.com>
2019-06-01 08:59:12 -0400

    Added markdown support

--- website/__init__.py
+++ website/__init__.py
@@ -6,6 +6,8 @@ from website.navbar import links
 from website.page import page
 from website.project import project, projects
 
+from markdown import markdown
+
 site = Flask(__name__)
 site.wsgi_app = ProxyFix(site.wsgi_app)
 
@@ -16,6 +18,10 @@ def inject_navlinks():
 @page.context_processor
 def inject_projects():
     return dict(projectlinks=projects)
+
+@site.template_filter('markdown')
+def markdown_filter(text):
+    return markdown(text)
 
 site.register_blueprint(page)
 site.register_blueprint(project, url_prefix='/project')
